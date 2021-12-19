81fa470f50a3e3602a509fb0233f01665aa1b891
Vika Likhacheva <91532599+nikalihach@users.noreply.github.com>
2021-12-19 17:24:44 +0300

    Add files via upload
    Усовершенствованная программа клиента (Л.р. 6)

---
+++ Client_Graphlib_Лр6/Client_graphlib.cpp
@@ -0,0 +1,236 @@
+﻿#include "Display.h"
+
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	
+	setlocale(LC_ALL, "RUS");
+	printf("Client\n");
+
+	char buff[1000];
+
+	if (WSAStartup(0x202, (WSADATA*)&buff[0]))
+	{
+		printf("WSAStartup error : % d \n", WSAGetLastError());
+	}
+
+	SOCKET my_sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (my_sock == INVALID_SOCKET)
+	{
+		printf("socket() error: % d \n", WSAGetLastError());
+		WSACleanup();
+	}
+
+	
+	Display display = Display(600, 600);
+	int x = 10;
+	int y = 10;
+	int lenght = 10;
+	
+	display.getSize(display.getWidth(), display.getHeight());
+
+	display.fillScreen(stoi("ffff", 0, 16));
+	//display.drawPixel(20,60, stoi(" 5FFB", 0, 16));
+   
+
+	display.drawLine(10, 60, 30,40, stoi("F9A6", 0, 16));
+	//display.drawRect(30, 140, 100, 80, stoi("F9A6", 0, 16));
+	//display.fillRect(20, 260, 100, 50, stoi("5FFB", 0, 16));
+	//display.drawEllipse(200, 50, 100, 80, stoi("749F", 0, 16));
+	//display.fillEllipse(200, 200, 100, 80, stoi("749F", 0, 16));
+	//display.drawText(100, 120, "Добро_пожаловать", stoi("5568", 0, 16), 1, 20);
+	//display.setOrientation(2);
+	display.setOrientation(1);
+	 
+
+	/*
+	while (1)
+	{
+		
+		int selection;
+		char Color[4];
+		int color;
+		int x0, x1, y0, y1, w, h, rx, ry;
+		
+		
+		cout << endl;
+		cout << "Выберите команду для отправки:" << endl;
+			cout << "1. fillScreen: color" << endl;
+			cout << "2. drawPixel: x0, y0, color" << endl;
+			cout << "3. drawLine: x0, y0, x1, y1, color" << endl;
+			cout << "4. drawRect: x0, y0, w, h, color" << endl;
+			cout << "5. fillRect: x0, y0, w, h, color" << endl;
+			cout << "6. drawEllipse: x0, y0, rx, ry, color" << endl;
+			cout << "7. fillEllipse: x0, y0, rx, ry, color" << endl;
+			cout << "\nВведите номер команды: ";
+		    cin >> selection;
+
+			switch (selection)
+			{
+			case 1:
+				
+				cout << "Подготовка команды 'fillScreen':"<<endl;
+				cout << "Введите параметр color (формат 0х0000):";
+				cin >> Color;
+
+				color = stoi(Color, 0, 16);
+
+				//uint_least16_t [0; 65535]
+				if (color < 0 || color> 65535)
+				{
+					cout << "Error: значение параметра color за диапазоном допустимых значений " << endl;
+				}
+
+				else
+				display.fillScreen(color);
+
+				break;
+
+			case 2:
+				cout << "Подготовка команды 'drawPixel':" << endl;
+				cout << "Введите парметр x0:";
+				cin >> x0;
+				cout << "Введите парметр y0:";
+				cin >> y0;
+				cout << "Введите параметр color (формат 0х0000):";
+				cin >> Color;
+
+				color = stoi(Color, 0, 16);
+				
+				if (color < 0 || color> 65535)
+				{
+					cout << "Error: значение параметра color за диапазоном допустимых значений " << endl;
+				}
+				else
+
+				display.drawPixel(x0, y0, color);
+				break;
+
+			case 3: 
+				cout << "Подготовка команды 'drawLine':" << endl; 
+				cout << "Введите парметр x0:";
+				cin >> x0;
+				cout << "Введите парметр y0:";
+				cin >> y0;
+				cout << "Введите парметр x1:";
+				cin >> x1;
+				cout << "Введите парметр y1:";
+				cin >> y1;
+				cout << "Введите параметр color (формат 0х0000):";
+				cin >> Color;
+				color = stoi(Color, 0, 16);
+				cout <<"Цвет: " <<color <<" Размер "<<sizeof(color)<< endl;
+				if (color < 0 || color> 65535)
+				{
+					cout << "Error: значение параметра color за диапазоном допустимых значений " << endl;
+				}
+
+				else
+				display.drawLine(x0, y0, x1, y1, color);
+				break;
+
+			case 4: 
+				cout << "Подготовка команды 'drawRect':" << endl;
+				cout << "Введите парметр x0:";
+				cin >> x0;
+				cout << "Введите парметр y0:";
+				cin >> y0;
+				cout << "Введите парметр w:";
+				cin >> w;
+				cout << "Введите парметр h:";
+				cin >> h;
+				cout << "Введите параметр color (формат 0х0000):";
+				cin >> Color;
+				color = stoi(Color, 0, 16);
+				if (color < 0 || color> 65535)
+				{
+					cout << "Error: значение параметра color за диапазоном допустимых значений " << endl;
+				}
+				else
+				display.drawRect(x0, y0, w, h, color);
+				break;
+
+			case 5:
+				cout << "Подготовка команды 'fillRect'" << endl; 
+				cout << "Введите парметр x0:";
+				cin >> x0;
+				cout << "Введите парметр y0:";
+				cin >> y0;
+				cout << "Введите парметр w:";
+				cin >> w;
+				cout << "Введите парметр h:";
+				cin >> h;
+				cout << "Введите параметр color (формат 0х0000):";
+				cin >> Color;
+				color = stoi(Color, 0, 16);
+				if (color < 0 || color> 65535)
+				{
+					cout << "Error: значение параметра color за диапазоном допустимых значений " << endl;
+				}
+				else
+				display.fillRect(x0, y0, w, h, color);
+				break;
+				
+			case 6:
+				cout << "Подготовка команды 'drawEllipse'" << endl; 
+				cout << "Введите парметр x0:";
+				cin >> x0;
+				cout << "Введите парметр y0:";
+				cin >> y0;
+				cout << "Введите парметр rx:";
+				cin >> rx;
+				cout << "Введите парметр ry:";
+				cin >> ry;
+				cout << "Введите параметр color (формат 0х0000):";
+				cin >> Color;
+				color = stoi(Color, 0, 16);
+
+				if (color < 0 || color> 65535)
+				{
+					cout << "Error: значение параметра color за диапазоном допустимых значений " << endl;
+				}
+				else
+				display.drawEllipse(x0, y0, rx, ry, color);
+				break;
+				
+			case 7:
+				cout << "Подготовка команды 'fillEllipse'" << endl; 
+				cout << "Введите парметр x0:";
+				cin >> x0;
+				cout << "Введите парметр y0:";
+				cin >> y0;
+				cout << "Введите парметр rx:";
+				cin >> rx;
+				cout << "Введите парметр ry:";
+				cin >> ry;
+				cout << "Введите параметр color (формат 0х0000):";
+				cin >> Color;
+				color = stoi(Color, 0, 16);
+				if (color < 0 || color> 65535)
+				{
+					cout << "Error: значение параметра color за диапазоном допустимых значений " << endl;
+				}
+
+				else
+
+				display.fillEllipse(x0, y0, rx, ry, color);
+
+				break;
+
+
+			default:
+				cout << "Ошибка: Введен не правильный номер команды" << endl;
+				break;
+			}
+	}
+	*/
+
+	return 0;
+}
---
+++ Client_Graphlib_Лр6/Display.h
@@ -0,0 +1,163 @@
+﻿#ifndef Display_h
+#define Display_h
+
+#include <iostream>
+#include "stdio.h"
+#include <stdlib.h>
+
+
+#include <string>
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib, "Ws2_32.lib")
+#include <winsock2.h>
+#include <windows.h>
+
+
+#include "GraphicsLib.h"
+
+using namespace std;
+
+int hexToRGB(uint16_t hexValue)
+{
+    unsigned r = (hexValue & 0xF800) >> 11;
+    unsigned g = (hexValue & 0x07E0) >> 5;
+    unsigned b = hexValue & 0x001F;
+
+    r = (r * 255) / 31;
+    g = (g * 255) / 63;
+    b = (b * 255) / 31;
+
+    return (r << 16) | (g << 8) | b;
+}
+
+
+class Display : public GraphicsLib
+{
+public:
+    Display(uint_least16_t w, uint_least16_t h, int PORT = 777, string SERVERADDR = "127.0.0.1") : GraphicsLib(w, h)
+    
+    {
+        my_sock = socket(AF_INET, SOCK_DGRAM, 0);
+        if (my_sock == INVALID_SOCKET)
+        {
+            printf("socket() error: %d\n", WSAGetLastError());
+            WSACleanup();
+        }
+
+        HOSTENT* hst;
+
+        dest_addr.sin_family = AF_INET;
+        dest_addr.sin_port = htons(PORT);
+
+        if (inet_addr(SERVERADDR.c_str()))
+            dest_addr.sin_addr.s_addr = inet_addr(SERVERADDR.c_str());
+        else
+            if (hst = gethostbyname(SERVERADDR.c_str()))
+                dest_addr.sin_addr.s_addr = ((unsigned long**)hst->h_addr_list)[0][0];
+            else
+            {
+                printf("Unknown host: %d\n", WSAGetLastError());
+                closesocket(my_sock);
+                WSACleanup();
+            }
+    };
+
+    void fillScreen(uint_least16_t color) {
+
+        int hexcolor = hexToRGB(color);
+
+        snprintf(buffer, buffer_length, "clear_display: %06x", hexcolor);
+        sendCommand(buffer);
+    };
+
+    void drawPixel(int_least16_t x0, int_least16_t y0, uint_least16_t color) {
+        int hexcolor = hexToRGB(color);
+        snprintf(buffer, buffer_length, "draw_pixel: %i %i %06x", x0, y0, hexcolor);
+        sendCommand(buffer);
+    };
+
+    void drawLine(int_least16_t x0, int_least16_t y0, int_least16_t x1, int_least16_t y1, uint_least16_t color) {
+        int hexcolor = hexToRGB(color);
+        snprintf(buffer, buffer_length, "draw_line: %i %i %i %i %06x", x0, y0, x1, y1, hexcolor);
+        sendCommand(buffer);
+    };
+
+    void drawRect(int_least16_t x0, int_least16_t y0, int_least16_t w, int_least16_t h, uint_least16_t color) {
+        int hexcolor = hexToRGB(color);
+        snprintf(  buffer, buffer_length, "draw_rectangle: %i %i %i %i %06x", x0, y0, w, h, hexcolor);
+        sendCommand(buffer);
+    };
+
+    void fillRect(int_least16_t x0, int_least16_t y0, int_least16_t w, int_least16_t h, uint_least16_t color) {
+        int hexcolor = hexToRGB(color);
+        snprintf(  buffer, buffer_length, "fill_rectangle: %i %i %i %i %06x", x0, y0, w, h, hexcolor);
+        sendCommand(buffer);
+    };
+
+    void drawEllipse(int_least16_t x0, int_least16_t y0, int_least16_t r_x, int_least16_t r_y, uint_least16_t color) {
+        int hexcolor = hexToRGB(color);
+        snprintf( buffer, buffer_length, "draw_ellipse: %i %i %i %i %06x", x0, y0, r_x, r_y, hexcolor);
+        sendCommand(buffer);
+    };
+
+    void fillEllipse(int_least16_t x0, int_least16_t y0, int_least16_t r_x, int_least16_t r_y, uint_least16_t color) {
+        int hexcolor = hexToRGB(color);
+        snprintf( buffer, buffer_length, "fill_ellipse: %i %i %i %i %06x", x0, y0, r_x, r_y, hexcolor);
+        sendCommand(buffer);
+    };
+
+
+    int_least16_t drawChar(int_least16_t x, int_least16_t y, char c, uint_least16_t color, uint_least16_t bg, uint_least8_t size) 
+    {
+        return 0;
+    }
+
+    int_least16_t drawChar(int_least16_t x, int_least16_t y, unsigned char c, uint_least16_t color, uint_least16_t bg, uint_least8_t size)  
+    {
+        return 0;
+    }
+        int_least16_t drawText(int_least16_t x, int_least16_t y, const char* s, uint_least16_t color, uint_least16_t bg, uint_least8_t size) 
+        {
+            int hexcolor = hexToRGB(color);
+            snprintf(buffer, buffer_length, "draw_text: %i %i %06x %i %i %s", x, y, hexcolor, bg, size, s);
+            sendCommand(buffer);
+            return 0;
+        }
+
+        void loadSprite(uint_least8_t index, int_least16_t width, int_least16_t height, char* data) 
+        {
+        }
+
+        void showSprite(uint_least8_t index, uint_least16_t x, uint_least16_t y) 
+        {
+        }
+
+
+        void setOrientation(uint_least8_t orientation)
+        {
+            snprintf(buffer, buffer_length, "set_orientation: %i", orientation);
+            sendCommand(buffer);
+        }
+
+        void getSize(uint_least16_t w, uint_least16_t h)
+        {
+            snprintf(buffer, buffer_length, "get_size: %i %i", w,h);
+            sendCommand(buffer);
+
+        }
+
+private:
+    static const unsigned char buffer_length = 1000;
+    char buffer[buffer_length];
+
+    sockaddr_in dest_addr;
+    SOCKET my_sock;
+
+    void sendCommand(const char* command) {
+        std::cout << "sendCommand: " << command << std::endl;
+        sendto(my_sock, &command[0], strlen(&command[0]), 0, (sockaddr*)&dest_addr, sizeof(dest_addr));
+    }
+};
+
+#endif 

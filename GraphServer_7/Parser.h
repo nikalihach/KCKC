cbb19a09e420bc3f3ba188b68511db6c31f3779e
Vika Likhacheva <91532599+nikalihach@users.noreply.github.com>
2021-12-28 10:42:45 +0300

    Update Parser.h

--- GraphServer_7/Parser.h
+++ GraphServer_7/Parser.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <string>
 using namespace std;
 
@@ -16,15 +15,21 @@ struct Command {
     int second = 9999999;
     int third = 9999999;
     int fourth = 9999999;
+
     int success = 9999999;
 
     int x, y, font_number, size = 9999999;
     char text[1000];
-    
+
     int orientation = 9999999;
 
     int width = 9999999;
     int height = 9999999;
+    
+    int w [256];
+    int h [256];
+
+    int load;
 
     int index = 9999999;
     char data[1000];
